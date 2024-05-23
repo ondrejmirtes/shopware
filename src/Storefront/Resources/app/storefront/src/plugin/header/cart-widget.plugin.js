dd90c7fee7e56410efdab90f387c19dea996fe4a
oskroblin Skroblin <goblin.coders@gmail.com>
2024-05-23 11:41:33 +0200

    NEXT-31669 - Optimized storefront js and wishlist controller api

--- src/Storefront/Resources/app/storefront/src/plugin/header/cart-widget.plugin.js
+++ src/Storefront/Resources/app/storefront/src/plugin/header/cart-widget.plugin.js
@@ -1,64 +1,59 @@
 import Plugin from 'src/plugin-system/plugin.class';
 import HttpClient from 'src/service/http-client.service';
-import Storage from 'src/helper/storage/storage.helper';
+import Cache from 'src/helper/cache.helper';
 
 export default class CartWidgetPlugin extends Plugin {
 
     static options = {
+        ttl: 120000,
+        // @deprecated tag:v6.7.0 - Will be removed, no more needed
         cartWidgetStorageKey: 'cart-widget-template',
+        // @deprecated tag:v6.7.0 - Will be removed, no more needed
         emptyCartWidgetStorageKey: 'empty-cart-widget',
     };
 
+    static key = 'sw-cart-cache';
+
     init() {
-
         this._client = new HttpClient();
+        this.url = window.router['frontend.checkout.info'];
 
-        this.insertStoredContent();
-        this.fetch();
+        this.load()
+
+        document.$emitter.subscribe('refresh-cart-widget', () => {
+            this.refresh();
+        });
+
+        document.$emitter.subscribe('store-cart-widget', (content) => {
+            this.store(content);
+        });
     }
 
-    /**
-     * reads the persisted content
-     * from the session cache an renders it
-     * into the element
-     */
-    insertStoredContent() {
-        // the page is initially always loaded with an empty cart
-        // save the empty cart widget, to reuse it when the cart is emptied
-        Storage.setItem(this.options.emptyCartWidgetStorageKey, this.el.innerHTML);
+    async load() {
+        const self = this;
 
-        const storedContent = Storage.getItem(this.options.cartWidgetStorageKey);
-        if (storedContent) {
-            this.el.innerHTML = storedContent;
-        }
-
-        this.$emitter.publish('insertStoredContent');
+        this.el.innerHTML = await Cache.get(CartWidgetPlugin.key, this.options.ttl, function() {
+            return self.fetchValue();
+        });
     }
 
-    /**
-     * Fetch the current cart widget template by calling the api
-     * and persist the response to the browser's session storage
-     */
-    fetch() {
-        this._client.get(window.router['frontend.checkout.info'], (content, response) => {
-            if (response.status >= 500) {
-                return;
-            }
+    refresh() {
+        this.fetchValue().then((value) => {
+            Cache.set(CartWidgetPlugin.key, this.options.ttl, value);
 
-            if (response.status === 204) {
-                Storage.removeItem(this.options.cartWidgetStorageKey);
-                const emptyCartWidget = Storage.getItem(this.options.emptyCartWidgetStorageKey);
-                if (emptyCartWidget) {
-                    this.el.innerHTML = emptyCartWidget;
+            this.el.innerHTML = value;
+        });
+    }
+
+    fetchValue() {
+        return new Promise((resolve) => {
+            this._client.get(this.url, (content, response) => {
+                if (response.status >= 500) {
+                    return;
                 }
 
-                return;
-            }
-
-            Storage.setItem(this.options.cartWidgetStorageKey, content);
-            this.el.innerHTML = content;
-
-            this.$emitter.publish('fetch', { content });
+                resolve(content);
+            })
         });
     }
 }
